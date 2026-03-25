c8d28956a0d482b66b98f22c3d071e5b7aaa9c6a
Franstio <franstiodiego01@gmail.com>
2026-03-26 00:02:28 +0700

    ok

---
+++ scanner-init.sql
@@ -0,0 +1,73 @@
+﻿CREATE TABLE scraptransaction(
+id integer primary key autoincrement,
+transaction_date text,
+login_date text,
+send_date text,
+badgeno text,
+container text,
+bin text,
+status text,
+host text,
+weightresult real,
+activity text,
+lastbadgeno text
+);
+
+CREATE TABLE login(
+id integer primary key autoincrement,
+username text,
+password text
+);
+CREATE table station(
+id integer primary key autoincrement,
+property text,
+datavalue text
+);
+CREATE table employee(
+id integer primary key autoincrement,
+employeename text,
+badgeno text,
+registerdate text,
+dispose integer,
+collection integer
+);
+CREATE TABLE binhost(
+bin TEXT primary key,
+weight real,
+wastetype text,
+maxweight real,
+lastfrombinname text,
+lastbadgeno TEXT,
+weightsystem text,
+binweight real,
+hostname TEXT,
+status TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS containerbin (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT ,
+    description TEXT ,
+    scrapitem_name TEXT ,
+    scraptype_name TEXT ,
+    weight NUMERIC ,
+    capacity NUMERIC ,
+    weightresult NUMERIC ,
+    weightsystem NUMERIC ,
+    wastestation_name TEXT ,
+    department_name TEXT ,
+    logindate TEXT,           
+    doorstatus INTEGER,       
+    lastfrombinname TEXT ,
+    url TEXT ,
+    scrapgroup_name TEXT ,
+    lastbadgeno TEXT ,
+    activity TEXT 
+);
+
+CREATE TABLE IF NOT EXISTS appdata (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    property TEXT,
+    datavalue TEXT
+);
